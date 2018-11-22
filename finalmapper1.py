5906925116d29c88a791300b54b96e4d632860c3
hxrrycollins <harrycol97@gmail.com>
2018-11-22 22:14:29 +0000

    .

--- finalmapper1.py
+++ finalmapper1.py
@@ -2,13 +2,11 @@
 """finalmapper.py"""
 import re
 import sys
-document = open("norvig.txt", "r")
 anagrams = {}
-for line in document:
+for line in sys.stdin:
     line = re.sub("[,.]", " ", line)
     line = line.lower().strip()
     words = line.split()
-
     for word in words:
         word = re.sub('[^A-Za-z]', '', word)
         holder = sorted(list(word))
